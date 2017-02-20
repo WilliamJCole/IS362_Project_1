6b9cc484fc4ac5be8dd8725fb70eb653d34c2326
William Cole <billy.cole@gmail.com>
2017-02-20 18:29:19 -0500

    preliminary post.

---
+++ Project1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+'''Flight Delays'''
+
+import pandas as pd
+import os
+
+Location = open(os.path.expanduser(r'~/IS362_Spring_2017/Project_1/delays.csv'))
+
+df = pd.read_csv(Location)
+
+print df.head()
+ 
+stack = df.stack()
+print stack 
