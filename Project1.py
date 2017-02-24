6f12bd4fbfeb7e37e32cb01d82a7725a7a1c8a75
William Cole <billy.cole@gmail.com>
2017-02-24 15:58:50 -0500

    Update

--- Project1.py
+++ Project1.py
@@ -2,6 +2,7 @@
 '''Flight Delays'''
 
 import pandas as pd
+import numpy as np
 import os
 
 Location = open(os.path.expanduser(r'~/IS362_Spring_2017/Project_1/delays.csv'))
@@ -9,6 +10,6 @@ Location = open(os.path.expanduser(r'~/IS362_Spring_2017/Project_1/delays.csv'))
 df = pd.read_csv(Location)
 
 print df.head()
- 
+
 stack = df.stack()
-print stack 
+print stack
