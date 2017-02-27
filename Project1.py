b5eb00812c281f4344b4c2d9ed541c00b2f7b5b8
William Cole <billy.cole@gmail.com>
2017-02-26 20:08:45 -0500

    git commit

--- Project1.py
+++ Project1.py
@@ -11,5 +11,24 @@ df = pd.read_csv(Location)
 
 print df.head()
 
-stack = df.stack()
-print stack
+sumofrows = data.set_index(['Airlines', 'Status'])
+sumofrows['Total'] = sumofrows.sum(axis=1)
+
+print(sumofrows)
+
+
+alaska_total = sumofrows.xs('Alaska').xs('delayed') + sumofrows.xs('Alaska').xs('on time')
+amwest_total = sumofrows.xs('AM West').xs('delayed') + sumofrows.xs('AM West').xs('on time')
+
+alaska_totaldelay = sumofrows.xs('Alaska').xs('delayed')
+alaska_delay = (alaska_totaldelay/alaska_total*100).round(2)
+
+amwest_totaldelay = sumofrows.xs('AM West').xs('delayed')
+amwest_delay = (amwest_totaldelay/amwest_total*100).round(2)
+
+print('Delays Stats: \n Alaska Airlines\n'
+      ,alaska_delay[0:-1], '\n --------------- \n AM West Airlines\n'
+      ,amwest_delay[0:-1], '\n --------------- \n'
+      ,'Alaska Airlines flights delayed: ',alaska_delay[-1],'%\n'
+      ,'AM West Airlines flights delayed: ',amwest_delay[-1],'%'
+     )
